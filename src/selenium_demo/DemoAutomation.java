2f26347b83ea9a2184526b00f1ca37520ab20e9c
a-programmer-guy <ctrl_kay@protonmail.com>
2022-08-06 18:56:47 -0600

    HOTFIX - add src file

---
+++ src/selenium_demo/DemoAutomation.java
@@ -0,0 +1,25 @@
+package selenium_demo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DemoAutomation {
+
+	public static void main(String[] args) {
+		/*
+		 * System.setProperty sets the system property specified by the key. For example,
+		 * project the key is the webdriver and the value must be the
+		 * location of where the driver is.
+		 * Ensure selenium driver jar file has been configured with the build path.
+		 */
+		System.setProperty("webdriver.chrome.driver", "/home/kay/Desktop/BrowserDrivers/chromedriver_linux64/chromedriver");
+		
+		ChromeDriver driver = new ChromeDriver();
+//		FirefoxDriver driver = new FirefoxDriver();
+//		EdgeDriver driver = new EdgeDriver();
+		driver.get("http://www.google.com");
+		
+	}
+
+}

2ccede9076e797d0b9d9f2ea96aca29eea632eba
a-programmer-guy <ctrl_kay@protonmail.com>
2022-08-07 11:45:33 -0600

    Add WebDriver interface
    This commit utilizes the WebDriver interface which will allow a
    much easier way to switch between different browsers for testing.
    
    Adding global variables for the browser and implimenting the WebDriver
    interface we can apply an if statement to determine which browser
    is being tested and the WebDriverManager can setup the corresponding
    driver appropriately.

--- maven/src/test/java/ui/LoginTest.java
+++ maven/src/test/java/ui/LoginTest.java
@@ -1,16 +1,31 @@
 package ui;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
 
+	public static String browser = "firefox"; // External configuration - XLS, CSV
+	public static WebDriver driver; // Global variable for driver
+	
 	public static void main(String[] args) {
-
-		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver();
+		
+		if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if(browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if(browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		
 		driver.get("http://www.saucedemo.com");
 		driver.manage().window().maximize();
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
